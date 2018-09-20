6d97fc32fa1f3e33cd913399a5b19e88767bf1b0
Katie <katierong121@163.com>
2018-09-20 14:59:23 +0800

    上传了代码
    Python编程入门-Homeworks

---
+++ Yufa/Task4.py
@@ -0,0 +1,42 @@
+# -*- coding: UTF-8 -*-
+"""
+下面的文件将会从csv文件中读取读取短信与电话记录，
+你将在以后的课程中了解更多有关读取文件的知识。
+"""
+import csv
+
+with open('texts.csv', 'r') as f:
+    reader = csv.reader(f)
+    texts = list(reader)
+
+with open('calls.csv', 'r') as f:
+    reader = csv.reader(f)
+    calls = list(reader)
+
+"""
+任务4:
+电话公司希望辨认出可能正在用于进行电话推销的电话号码。
+找出所有可能的电话推销员:
+这样的电话总是向其他人拨出电话，
+但从来不发短信、接收短信或是收到来电
+
+
+请输出如下内容
+"These numbers could be telemarketers: "
+<list of numbers>
+电话号码不能重复，每行打印一条，按字典顺序排序后输出。
+"""
+
+list_a = []
+
+call, beCalled, time, last = zip(*calls)
+msg, beMsged, time = zip(*texts)
+
+for i in calls:
+    if i[0] not in beCalled and i[0] not in msg and i[0] not in beMsged:
+        list_a.append((i[0]))
+
+list_b = set(list_a)
+print("These numbers could be telemarketers: ")
+for x in sorted((list_b)):
+    print(x)
